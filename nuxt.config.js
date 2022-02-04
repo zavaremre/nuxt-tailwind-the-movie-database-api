600f8cf8d0469042788e42db9548f7b4777ea6af
EMRE ZAVAR <zavaremre@gmail.com>
2022-02-04 16:40:59 +0300

    apple-mobile-web-app-capable

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,11 +9,16 @@ export default {
       { charset: 'utf-8' },
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0',
+        content: 'target-densitydpi=device-dpi, width=device-width, user-scalable=no, maximum-scale=1, minimum-scale=1',
       },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
+      {
+        name: 'apple-mobile-web-app-capable',
+        content: 'yes',
+      },
     ],
+
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
