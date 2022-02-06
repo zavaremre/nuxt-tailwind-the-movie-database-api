9246e23194d9e3648e893b70f2fb251b9758c897
EMRE ZAVAR <zavaremre@gmail.com>
2022-02-06 17:50:07 +0300

    dakmode

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,20 +8,10 @@
 <script>
 export default {
   head: {
-    // title: 'my website title',
     htmlAttrs: {
       class: '',
       lang: 'tr',
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'my website description',
-      },
-    ],
     link: [
       {
         rel: 'preconnect',
