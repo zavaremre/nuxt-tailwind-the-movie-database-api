38e1f33f7f1838d53999c666ae1d4a27c6005441
EMRE ZAVAR <zavaremre@gmail.com>
2022-02-06 17:45:20 +0300

    dakmode

--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,7 @@
         </template>
       </List>
       <div class="flex justify-center items-center w-full">
-        <router-link :to="{ path: 'list', query: { data: 'trending/person/day', person: true } }" class="inline-flex justify-center items-center px-5 py-1.5 mt-5 text-white font-bold rounded-2xl border border-gray-100">Tümünü Gör</router-link>
+        <router-link :to="{ path: 'list', query: { data: 'trending/person/day', person: true } }" class="inline-flex justify-center items-center px-5 py-1.5 mt-5 dark:text-white text-gray-900 font-bold rounded-2xl border border-gray-100">Tümünü Gör</router-link>
       </div>
     </section>
     <section class="py-5 px-2.5 border-b border-gray-100 dark:border-gray-900">
@@ -32,7 +32,7 @@
         </template>
       </List>
       <div class="flex justify-center items-center w-full">
-        <router-link :to="{ path: 'list', query: { data: 'movie/now_playing' } }" class="inline-flex justify-center items-center px-5 py-1.5 mt-5 text-white font-bold rounded-2xl border border-gray-100">Tümünü Gör</router-link>
+        <router-link :to="{ path: 'list', query: { data: 'movie/now_playing' } }" class="inline-flex justify-center items-center px-5 py-1.5 mt-5 dark:text-white text-gray-900 font-bold rounded-2xl border border-gray-100">Tümünü Gör</router-link>
       </div>
     </section>
 
@@ -44,7 +44,7 @@
         </template>
       </List>
       <div class="flex justify-center items-center w-full">
-        <router-link :to="{ path: 'list', query: { data: 'movie/popular' } }" class="inline-flex justify-center items-center px-5 py-1.5 mt-5 text-white font-bold rounded-2xl border border-gray-100">Tümünü Gör</router-link>
+        <router-link :to="{ path: 'list', query: { data: 'movie/popular' } }" class="inline-flex justify-center items-center px-5 py-1.5 mt-5 dark:text-white text-gray-900 font-bold rounded-2xl border border-gray-100">Tümünü Gör</router-link>
       </div>
     </section>
     <section class="py-5 px-2.5 border-b border-gray-100 dark:border-gray-900">
@@ -55,7 +55,7 @@
         </template>
       </List>
       <div class="flex justify-center items-center w-full">
-        <router-link :to="{ path: 'list', query: { data: 'movie/top_rated' } }" class="inline-flex justify-center items-center px-5 py-1.5 mt-5 text-white font-bold rounded-2xl border border-gray-100">Tümünü Gör</router-link>
+        <router-link :to="{ path: 'list', query: { data: 'movie/top_rated' } }" class="inline-flex justify-center items-center px-5 py-1.5 mt-5 dark:text-white text-gray-900 font-bold rounded-2xl border border-gray-100">Tümünü Gör</router-link>
       </div>
     </section>
     <section class="py-5 px-2.5 border-b border-gray-100 dark:border-gray-900">
@@ -66,7 +66,7 @@
         </template>
       </List>
       <div class="flex justify-center items-center w-full">
-        <router-link :to="{ path: 'list', query: { data: 'movie/upcoming' } }" class="inline-flex justify-center items-center px-5 py-1.5 mt-5 text-white font-bold rounded-2xl border border-gray-100">Tümünü Gör</router-link>
+        <router-link :to="{ path: 'list', query: { data: 'movie/upcoming' } }" class="inline-flex justify-center items-center px-5 py-1.5 mt-5 dark:text-white text-gray-900 font-bold rounded-2xl border border-gray-100">Tümünü Gör</router-link>
       </div>
     </section>
   </section>
