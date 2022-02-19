5fa0b8ebb2245f1b58635a3592c3272c796acf25
EMRE ZAVAR <zavaremre@gmail.com>
2022-02-19 20:24:54 +0300

    update

--- components/Header/Index.vue
+++ components/Header/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="relative py-5 px-4 border-b border-gray-100 dark:border-gray-900 w-full">
-    <button v-if="$route.fullPath !== '/'" class="absolute left-0 top-0 p-4 h-16 w-16" @click="$router.go(-1)">
+    <button v-if="$route.fullPath !== '/'" class="absolute left-0 top-0 p-4 h-16 w-16 z-10" @click="$router.go(-1)">
       <img :src="require('~/assets/images/chevron-left.svg')" class="w-6 h-6 dark:invert" alt="" />
     </button>
 
@@ -9,7 +9,7 @@
         <router-link to="/" class="font-bold text-xl">Filmografi</router-link>
       </div>
     </section>
-    <button class="absolute right-0 top-0 p-4 h-16 w-16" @click="reload">
+    <button class="absolute right-0 top-0 p-4 h-16 w-16 z-10" @click="reload">
       <img :src="require('~/assets/images/reload.svg')" class="w-6 h-6 dark:invert" alt="" />
     </button>
     <section class="container">
@@ -21,6 +21,9 @@
 </template>
 <script>
 export default {
+  get currentRoute() {
+    return this.history && this.history.current
+  },
   methods: {
     reload() {
       this.$router.go(this.$router.currentRoute)
--- pages/list/_url.vue
+++ pages/list/_url.vue
@@ -1,30 +1,58 @@
 <template>
-  <div class="container">
-    <section class="py-5 px-5 border-b border-gray-100 dark:border-gray-900">
-      <Titlebar v-if="$route.query.categorie" :title="$route.query.title" />
-      <Titlebar v-else title="TÜMÜ" />
-
-      <div class="grid grid-cols-3 sm:grid-cols-6 gap-x-4">
-        <div v-for="item in list" :key="item.id" class="mb-4">
-          <Person v-if="$route.query.type === 'trending/person/day'" :person="item" custom-style="" image-style="h-auto rounded-lg bg-none" no-image-style="rounded-lg h-32" />
-          <Poster v-else :movie="item" />
+  <span>
+    <section v-if="genre !== null && genreId" class="bg-gray-100 dark:bg-stone-800 py-4">
+      <div class="container">
+        <div class="filter flex sm:flex-row justify-center items-center w-full px-4 sm:px-0">
+          <select v-model="shortBy" class="bg-white dark:bg-stone-900 rounded-3xl focus:outline-0 w-full py-2.5 px-4 mr-3 text-md" @change="filter">
+            <option value="popularity.asc">Popülerlik -(Düşükten Yükseğe)</option>
+            <option value="popularity.desc">Popülerlik - (Yüksekten Düşüğe)</option>
+            <option value="release_date.asc">Yayın Tarihi - (Düşükten Yükseğe)</option>
+            <option value="release_date.desc">Yayın Tarihi - (Yüksekten Düşüğe)</option>
+            <option value="revenue.asc">Düşük Bütçeli</option>
+            <option value="revenue.desc">Yüksek Bütçeli</option>
+            <option value="primary_release_date.asc">Çıkış Tarihi - (Düşükten Yükseğe)</option>
+            <option value="primary_release_date.desc">Çıkış Tarihi - (Yüksekten Düşüğe)</option>
+            <option value="original_title.asc">Orjinal İsme Göre -(Düşükten Yükseğe)</option>
+            <option value="original_title.desc">Orjinal İsme Göre - (Yüksekten Düşüğe)</option>
+            <option value="vote_average.asc">Oy Ortalaması (Düşükten Yükseğe)</option>
+            <option value="vote_average.desc">Oy Ortalaması (Yüksekten Düşüğe)</option>
+            <option value="vote_count.asc">Oy Sayısı - (Düşükten Yükseğe)</option>
+            <option value="vote_count.desc">Oy Sayısı - (Yüksekten Düşüğe)</option>
+          </select>
+          <input v-model="year" class="bg-white dark:bg-stone-900 rounded-3xl focus:outline-0 w-full py-2.5 px-4 ml-3 text-md" type="number" name="" placeholder="Film Yılı" @change="filter" />
         </div>
       </div>
-      <!-- <div v-if="page !== 1 || total_pages !== page" class="flex justify-between items-center w-full py-3">
-        <div><button v-if="total_pages !== 1 && page !== 1" class="px-5 py-1.5 border border-gray-100 rounded-2xl" @click="pageChangeNegative">Önceki Sayfa</button></div>
-        {{ page }} / {{ total_pages }}
-        <div><button v-if="total_pages >= page" class="px-5 py-1.5 border border-gray-100 rounded-2xl" @click="pageChange">Sonraki Sayfa</button></div>
-      </div> -->
     </section>
-    <div v-if="list.length" v-observe-visibility="handleScrolledToBottom"></div>
-    <!-- <div v-if="page >= total_pages" class="flex justify-center items-center w-full py-3">Hepsi Bu Kadar :)</div> -->
-  </div>
+    <div class="container">
+      <section class="py-5 px-5 border-b border-gray-100 dark:border-gray-900">
+        <Titlebar v-if="$route.query.categorie" :title="$route.query.title" />
+        <Titlebar v-else title="TÜMÜ" />
+
+        <div class="grid grid-cols-3 sm:grid-cols-6 gap-x-4">
+          <div v-for="item in list" :key="item.id" class="mb-4">
+            <Person v-if="$route.query.type === 'trending/person/day'" :person="item" custom-style="" image-style="h-auto rounded-lg bg-none" no-image-style="rounded-lg h-32" />
+            <Poster v-else :movie="item" />
+          </div>
+        </div>
+        <!-- <div v-if="page !== 1 || total_pages !== page" class="flex justify-between items-center w-full py-3">
+          <div><button v-if="total_pages !== 1 && page !== 1" class="px-5 py-1.5 border border-gray-100 rounded-2xl" @click="pageChangeNegative">Önceki Sayfa</button></div>
+          {{ page }} / {{ total_pages }}
+          <div><button v-if="total_pages >= page" class="px-5 py-1.5 border border-gray-100 rounded-2xl" @click="pageChange">Sonraki Sayfa</button></div>
+        </div> -->
+      </section>
+      <div v-if="list.length" v-observe-visibility="handleScrolledToBottom"></div>
+      <!-- <div v-if="page >= total_pages" class="flex justify-center items-center w-full py-3">Hepsi Bu Kadar :)</div> -->
+    </div>
+  </span>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      year: null,
+      with_keywords: '',
+      shortBy: 'popularity.desc',
       genre: this.$route.params.url.split('-').pop(),
       genreId: this.$route.query.id,
       list: [],
@@ -36,7 +64,7 @@ export default {
     if (this.genre !== null && this.$route.query.type !== 'trending/person/day') {
       this.list = []
       try {
-        this.$axios.get(`/api?action=discover/movie?with_genres=${this.genreId}&language=tr-TR&page=${this.page}`).then((response) => {
+        this.$axios.get(`/api?action=discover/movie?with_genres=${this.genreId}&language=tr-TR&sort_by=${this.shortBy}&page=${this.page}`).then((response) => {
           this.list.push(...response.data.results)
 
           this.total_pages = response.data.total_pages
@@ -58,6 +86,16 @@ export default {
     $route: '$fetch',
   },
   methods: {
+    filter() {
+      this.list = []
+      try {
+        this.$axios.get(`/api?action=discover/movie?with_genres=${this.genreId}&language=tr-TR&primary_release_year=${this.year}&sort_by=${this.shortBy}&with_keywords=${this.with_keywords}&page=${this.page}`).then((response) => {
+          this.list.push(...response.data.results)
+
+          this.total_pages = response.data.total_pages
+        })
+      } catch (error) {}
+    },
     handleScrolledToBottom(isVisible) {
       if (!isVisible) {
         return
@@ -72,7 +110,7 @@ export default {
     fetch() {
       if (this.genre !== null && this.$route.query.type !== 'trending/person/day') {
         try {
-          this.$axios.get(`/api?action=discover/movie?with_genres=${this.genreId}&language=tr-TR&page=${this.page}`).then((response) => {
+          this.$axios.get(`/api?action=discover/movie?with_genres=${this.genreId}&language=tr-TR&sort_by=${this.shortBy}&page=${this.page}`).then((response) => {
             this.list.push(...response.data.results)
 
             this.total_pages = response.data.total_pages
